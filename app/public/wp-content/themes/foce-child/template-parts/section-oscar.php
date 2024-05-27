5474f87e5ded48325d96e9ed95f20c6a7926dff1
StasiyaN <ana.nyzhalska@gmail.com>
2024-05-27 11:02:56 +0200

    creation de la section oscar

---
+++ app/public/wp-content/themes/foce-child/template-parts/section-oscar.php
@@ -0,0 +1,12 @@
+
+<section id="oscar">
+    <!-- <div class="oscars__content"> -->
+        <article class="oscar__article">
+        <h3 class="oscar__title">
+        Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !
+        </h3>
+        </article>
+        <!-- </div> -->
+        <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/oscars.png"; ?>" alt="" class="oscar__img">
+    
+</section>
