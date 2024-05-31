fb22a62caf659b155c03e271c7a6ebd3e72ad95e
StasiyaN <ana.nyzhalska@gmail.com>
2024-05-31 18:41:30 +0200

    creation du template slider pour lister les images popur le slider + ajout du lien dans le functions php pour la creation du slider

--- app/public/wp-content/themes/foce-child/functions.php
+++ app/public/wp-content/themes/foce-child/functions.php
@@ -10,8 +10,13 @@ function theme_enqueue_styles() {
     // Enqueue child script
     wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), null, true );
     wp_enqueue_script ('parallax-script', get_stylesheet_directory_uri () . '/assets/js/parallax.js', array('jquery'), null, true);
-//simple parallax
-wp_enqueue_script( 'simple-parallaxe', 'https://cdnjs.cloudflare.com/ajax/libs/simple-parallax-js/5.6.2/simpleParallax.min.js', array(), true );
+    //simple parallax
+    wp_enqueue_script( 'simple-parallaxe', 'https://cdnjs.cloudflare.com/ajax/libs/simple-parallax-js/5.6.2/simpleParallax.min.js', array(), true );
+    //swiper slider
+    wp_enqueue_style('swiper-slider', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), true);
+    wp_enqueue_script('swiper-slider-script', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), true);
+
+
 }
 
 
---
+++ app/public/wp-content/themes/foce-child/template-parts/slider.php
@@ -0,0 +1,31 @@
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <div class="swiper-slide__image">
+                <img src="<?php echo get_stylesheet_directory_uri() . "/asstes/images/chats/Kawaneko.png";?>" alt="">
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="swiper-slide__image">
+                <img src="<?php echo get_stylesheet_directory_uri() . "/asstes/images/chats/Orenjiro.png";?>" alt="">
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="swiper-slide__image">
+                <img src="<?php echo get_stylesheet_directory_uri() . "/asstes/images/chats/Pinku.png";?>" alt="">
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="swiper-slide__image">
+                <img src="<?php echo get_stylesheet_directory_uri() . "/asstes/images/chats/Tenshi.png";?>" alt="">
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="swiper-slide__image">
+                <img src="<?php echo get_stylesheet_directory_uri() . "/asstes/images/chats/Jaakuna.png";?>" alt="">
+            </div>
+        </div>
+       
+    </div>
+
+</div>
