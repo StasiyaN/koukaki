99f94f0d16beeac0ece7c16abd67195a646df00f
StasiyaN <ana.nyzhalska@gmail.com>
2024-06-08 19:04:09 +0200

    creation d'animation float en cour

--- app/public/wp-content/themes/foce-child/assets/js/main.js
+++ app/public/wp-content/themes/foce-child/assets/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //declaration de variables
     const menuIcon =  document.querySelector('.menu-icon');
     const burgerMenuContent = document.querySelector('.burger-menu');
-    const links = burgerMenuContent.querySelectorAll('.burger-menu li a');
+    const links = burgerMenuContent.querySelectorAll('li a');
   
    
     //burger menu
@@ -30,7 +30,16 @@ document.addEventListener('DOMContentLoaded', function() {
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 anchors.forEach(anchor => {
-    console.log(anchor);
+    anchor.addEventListener('click', event => {
+        event.preventDefault();//cancellin defaut behaviour of the link redirection
+
+        const blockID = anchor.getAttribute('href').substring(1);//getting rid of the #
+        this.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+
+        });
+        });
 });
 
 });
