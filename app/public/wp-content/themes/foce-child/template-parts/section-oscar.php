a90c893494309a95dda23056c2b463beaccc9c75
StasiyaN <ana.nyzhalska@gmail.com>
2024-06-16 18:16:34 +0200

    modification de la stucture de oscar title -> envelevemnt des spans pour ne pas couper la phrase

--- app/public/wp-content/themes/foce-child/template-parts/section-oscar.php
+++ app/public/wp-content/themes/foce-child/template-parts/section-oscar.php
@@ -3,7 +3,7 @@
     <!-- <div class="oscars__content"> -->
         <article class="oscar__article">
             <h3 class="oscar__title">
-                <span>Fleurs d’oranger & chats errants </span><br><span>est nominé aux Oscars Short</span><br><span>Film Animated de 2022!</span>
+                <span>Fleurs d’oranger & chats errants <br> est nominé aux Oscars Short<br>Film Animated de 2022!</span>
             </h3>
         </article>
         <!-- </div> -->
