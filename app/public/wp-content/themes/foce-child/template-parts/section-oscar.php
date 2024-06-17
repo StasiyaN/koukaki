a1d2563520bf37495f7855522ae3f7e765ba2acf
StasiyaN <ana.nyzhalska@gmail.com>
2024-06-17 07:47:09 +0200

    avancement sur la vers responsive front page il reste quelques modifications niveau bannier et sunflower supprimé pour le moment car conflit

--- app/public/wp-content/themes/foce-child/template-parts/section-oscar.php
+++ app/public/wp-content/themes/foce-child/template-parts/section-oscar.php
@@ -1,12 +1,9 @@
 
 <section id="oscar">
-    <!-- <div class="oscars__content"> -->
         <article class="oscar__article">
             <h3 class="oscar__title">
-                <span>Fleurs d’oranger & chats errants <br> est nominé aux Oscars Short<br>Film Animated de 2022!</span>
+               <span class="oscar__text">Fleurs d’oranger & chats errants <br> est nominé aux Oscars Short<br>Film Animated de 2022!</span>
             </h3>
         </article>
-        <!-- </div> -->
-        <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/oscars.png"; ?>" alt="" class="oscar__img">
-    
+            <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/oscars.png"; ?>" alt="" class="oscar__img"> 
 </section>
